3d22411e6da5caa893c0202eeddfbb44bddcf8ce
Josh Oldmixon <40932443+JoshOldmixon@users.noreply.github.com>
2022-11-09 16:27:18 -0500

    Add files via upload

---
+++ WRDS_Modeling/Main.py
@@ -0,0 +1,16 @@
+from Tools.WRDSData import wrdsData
+from Tools.Models.LinearRegression import linearRegression
+from Tools.Models.LGBMRegressor import lightGBMRegressor
+from datetime import date
+from dateutil.relativedelta import relativedelta
+
+
+start = (date.today()- relativedelta(years=1)).strftime("%m/%d/%Y")
+
+data = wrdsData('AAPL',start).table
+
+linReg = linearRegression(data)
+lgbmreg = lightGBMRegressor(data)
+
+print ('Linear Regression:',linReg.score())
+print ('Light GBM Regressor:',lgbmreg.score())
---
+++ WRDS_Modeling/Tools/WRDSData.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from datetime import date
+import wrds as w
+from dateutil.relativedelta import relativedelta
+
+class wrdsData:
+
+    def __init__(self,symbol,start):
+        
+        #               Change Login To Use
+        
+        wrds = w.Connection(wrds_username=*******)
+        
+        
+        p = {'symbol': symbol, 'start': start}
+        ohlc = wrds.raw_sql("""select prcod, prchd, prcld, prccd, tic, datadate
+                                    from comp_na_daily_all.secd
+                                    where tic = %(symbol)s
+                                    and datadate>=%(start)s""",
+                                    params=p,
+                                    date_cols=['date'])
+
+        df = pd.DataFrame(ohlc).reset_index().drop(columns='index').rename(columns={"prcod": "open", "prchd": "high", "prcld": "low", "prccd": "close"})
+        
+        self.table = df
